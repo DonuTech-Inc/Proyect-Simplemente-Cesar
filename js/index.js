3462dd20148910f10110784c53752a872c4e27f1
NicollLaguna <nicky7u7lagos@gmail.com>
2024-09-12 17:04:04 -0500

    Secciones sin queries

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const texto = document.getElementById('bienvenido-texto');
     const textoContent = texto.innerHTML;
     texto.innerHTML = '';
@@ -19,4 +19,66 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     typeWriter();
+});
+
+let cards = document.querySelectorAll('.card');
+let currentIndex = 0; // El índice de la tarjeta en foco
+
+function updateCarousel() {
+    cards.forEach((card, index) => {
+        card.classList.remove('focus');
+        if (index === currentIndex) {
+            card.classList.add('focus');
+        }
+    });
+}
+
+function prev() {
+    currentIndex = (currentIndex === 0) ? cards.length - 1 : currentIndex - 1;
+    updateCarousel();
+}
+
+function next() {
+    currentIndex = (currentIndex === cards.length - 1) ? 0 : currentIndex + 1;
+    updateCarousel();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const faqQuestions = document.querySelectorAll('.faq-question');
+
+    faqQuestions.forEach(function (question) {
+        question.addEventListener('click', function () {
+            const answer = this.nextElementSibling;
+            const isVisible = answer.style.display === 'block';
+
+            // Hide all other answers
+            document.querySelectorAll('.faq-answer').forEach(function (ans) {
+                ans.style.display = 'none';
+            });
+
+            // Toggle current answer
+            if (!isVisible) {
+                answer.style.display = 'block';
+            }
+        });
+    });
+});
+
+
+// Mostrar la flecha cuando se hace scroll hacia abajo
+window.onscroll = function() {
+    const backToTopButton = document.getElementById("backToTop");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+
+// Redirigir a la parte superior cuando se hace clic en la flecha
+document.getElementById("backToTop").addEventListener("click", function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Desplazamiento suave
+    });
 });
